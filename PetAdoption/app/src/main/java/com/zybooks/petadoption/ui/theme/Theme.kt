39b1687e69791e1a7d409716aa193d057bb76d1e
Alok Shah <alok.shah620@gmail.com>
2025-02-08 06:16:35 -0800

    list screen and details

--- PetAdoption/app/src/main/java/com/zybooks/petadoption/ui/theme/Theme.kt
+++ PetAdoption/app/src/main/java/com/zybooks/petadoption/ui/theme/Theme.kt
@@ -5,6 +5,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
+import com.zybooks.petadoption.ui.theme.PetAdoptionTheme
 
 
 private val LightColors = lightColorScheme(
